c0819828cb48c44b75698bc1d2f681f7ebcc8fca
YanaSem <yana.semenenya@mail.ru>
2021-05-08 13:00:04 +0300

    no message

--- yanapy/interpretators/baseinterpretator.py
+++ yanapy/interpretators/baseinterpretator.py
@@ -1,3 +1,5 @@
+import shap 
+
 class BaseInterpretator:
     """
     Базовый класс интерпретатора
@@ -9,11 +11,26 @@ class BaseInterpretator:
         :param model: Модель для интерпретации
         """
         self.__model = model
+    
+    def fit_shap(self):
+        self.__explainer = shap.TreeExplainer(self.__model)
+        return
 
-    def interpret_1(self, data):
+    def shap(self, data, type = 'summary_plot', num_features = None):
         """
         Плейсхолдер для метода интепретации
-        :param data: Данные, на которых будет производиться интерпретация
+        :param data: Данные, на которых построенна модель. Используются для отдельных видоп интепретации
         :return: Возвращает результат интепретации
         """
-        pass
+        if type == 'summary_plot':
+            shap_values = self.__explainer.shap_values(data)
+            return shap.summary_plot(shap_values, data, max_display = num_features)
+        
+        if type == 'summary_bar_plot':
+            shap_values = self.__explainer.shap_values(data)
+            return shap.summary_plot(shap_values, data, plot_type='bar', max_display = num_features)
+        
+        if type == 'individual_plot':
+            shap.initjs()
+            shap_value_sample = self.__explainer.shap_values(data)
+            return shap.force_plot(self.__explainer.expected_value, shap_value_sample, data)
